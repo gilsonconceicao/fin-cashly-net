ecf2ca3f9d8b3104a6171a1c9a0a07aa507221c8
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-11-17 00:42:38 -0300

    feat: add comments swagger in enums

--- src/FinCashly.Domain/Enums/AccountTypeEnum.cs
+++ src/FinCashly.Domain/Enums/AccountTypeEnum.cs
@@ -4,18 +4,33 @@ namespace FinCashly.Domain.Enums;
 
 public enum AccountTypeEnum
 {
+    /// <summary>
+    /// Conta corrente
+    /// </summary>
     [Description("Conta corrente")]
     Checking = 1,
-
+    
+    /// <summary>
+    /// Conta poupança
+    /// </summary>
     [Description("Conta poupança")]
     Savings = 2,
 
+    /// <summary>
+    /// Conta de investimento
+    /// </summary>
     [Description("Conta de investimento")]
     Investment = 3,
-
+    
+    /// <summary>
+    /// Dinheiro em espécie
+    /// </summary>
     [Description("Dinheiro em espécie")]
     Cash = 4,
-    
+
+    /// <summary>
+    /// Cartão de crédito
+    /// </summary>
     [Description("Cartão de crédito")]
     Credit = 5
 }
--- src/FinCashly.Domain/Enums/TransactionTypeEnum.cs
+++ src/FinCashly.Domain/Enums/TransactionTypeEnum.cs
@@ -4,9 +4,15 @@ namespace FinCashly.Domain.Enums;
 
 public enum TransactionTypeEnum
 {
+    /// <summary>
+    /// Saída
+    /// </summary>
     [Description("Saída")]
     Expense = 1,
-    
+
+    /// <summary>
+    /// Entrada
+    /// </summary>
     [Description("Entrada")]
     Income = 2
 }
