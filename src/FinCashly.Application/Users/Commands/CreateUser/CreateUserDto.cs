396166996bec85c00b3e57519b5bef9ffa90ee3b
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-11-20 01:17:18 -0300

    revert: possible add accounts and goals by creted user flow

--- src/FinCashly.Application/Users/Commands/CreateUser/CreateUserDto.cs
+++ src/FinCashly.Application/Users/Commands/CreateUser/CreateUserDto.cs
@@ -1,5 +1,7 @@
-namespace FinCashly.Application.Users.Commands.CreateUser;
+using FinCashly.Application.Accounts.Commands.CreateAccount;
+using FinCashly.Application.Goals.Commands.CreateAccount;
 
+namespace FinCashly.Application.Users.Commands.CreateUser;
 #nullable disable
 public class CreateUserDto
 {
@@ -12,5 +14,15 @@ public class CreateUserDto
     /// Email do usuário
     /// </summary>
     public string Email { get; set; }
+
+    /// <summary>
+    /// Contas bancárias do usuário
+    /// </summary>
+    public List<CreateAccountDto> Accounsts { get; set; } = new List<CreateAccountDto>();
+
+    /// <summary>
+    /// Objetivos financeiros do usuário
+    /// </summary>
+    public List<CreateGoalDto> Goals { get; set; } = new List<CreateGoalDto>();
 
 }
